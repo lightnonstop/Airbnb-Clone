93e4e4d016f3659df1524b663beaabff32ba27d6
lightnonstop <umichaeledet003@gmail.com>
2023-04-24 14:21:43 +0000

    added page logo

---
+++ app/components/Navbar/Logo.tsx
@@ -0,0 +1,15 @@
+'use client';
+import Image from 'next/image';
+import React from 'react';
+import { useRouter } from 'next/navigation';
+
+const Logo = () => {
+    const router = useRouter()
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Logo
